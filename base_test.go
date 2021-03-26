256e62655ea06018581b53a72f6655fd127600b8
Isak Hellemo <isak.o.hellemo@uit.no>
2021-03-27 00:54:44 +0100

    Fixed tests

--- base_test.go
+++ base_test.go
@@ -1,34 +1,33 @@
 package main
 
 import (
-	"log"
 	"testing"
 )
 
-func assertEqual(a, b string) {
+func assertEqual(a, b string, t *testing.T) {
 	if a != b {
-		log.Fatalln(a, "not equal to: ", b, len(a), len(b))
+		t.Fatal(a, "not equal to: ", b)
 	}
 }
 
 func Test_encode(t *testing.T) {
 
-	assertEqual(encodeString(""), "")
-	assertEqual(encodeString("f"), "Zg==")
-	assertEqual(encodeString("fo"), "Zm8=")
-	assertEqual(encodeString("foo"), "Zm9v")
-	assertEqual(encodeString("foob"), "Zm9vYg==")
-	assertEqual(encodeString("fooba"), "Zm9vYmE=")
-	assertEqual(encodeString("foobar"), "Zm9vYmFy")
+	assertEqual(encodeString(""), "", t)
+	assertEqual(encodeString("f"), "Zg==", t)
+	assertEqual(encodeString("fo"), "Zm8=", t)
+	assertEqual(encodeString("foo"), "Zm9v", t)
+	assertEqual(encodeString("foob"), "Zm9vYg==", t)
+	assertEqual(encodeString("fooba"), "Zm9vYmE=", t)
+	assertEqual(encodeString("foobar"), "Zm9vYmFy", t)
 }
 
 func Test_Decode(t *testing.T) {
 
-	assertEqual("", string(decodeString("")))
-	assertEqual("f", string(decodeString("Zg==")))
-	assertEqual("fo", string(decodeString("Zm8=")))
-	assertEqual("foo", string(decodeString("Zm9v")))
-	assertEqual("foob", string(decodeString("Zm9vYg==")))
-	assertEqual("fooba", string(decodeString("Zm9vYmE=")))
-	assertEqual("foobar", string(decodeString("Zm9vYmFy")))
+	assertEqual("", string(decodeString("")), t)
+	assertEqual("f", string(decodeString("Zg==")), t)
+	assertEqual("fo", string(decodeString("Zm8=")), t)
+	assertEqual("foo", string(decodeString("Zm9v")), t)
+	assertEqual("foob", string(decodeString("Zm9vYg==")), t)
+	assertEqual("fooba", string(decodeString("Zm9vYmE=")), t)
+	assertEqual("foobar", string(decodeString("Zm9vYmFy")), t)
 }
